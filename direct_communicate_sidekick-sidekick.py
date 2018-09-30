2c0b156bd4085737a91994c1deafdf05c671c0af
Ghost <ghost@Ghosts-MacBook-Pro.local>
2018-09-30 22:00:31 +0200

    d

--- direct_communicate_sidekick-sidekick.py
+++ direct_communicate_sidekick-sidekick.py
@@ -76,7 +76,7 @@ async def on_message(message):
             stdout, stderr = command(system_command)
             embed = discord.Embed(title='System Command', description='Host', colour=0xDEADBF)
             if stdout:
-                embed.add_field(name="stdout", value='stdout')
+                embed.add_field(name="stdout", value=stdout.read())
             if stderr:
                 embed.add_field(name="stderr", value='stderr')
             if not stdout and not stderr:
