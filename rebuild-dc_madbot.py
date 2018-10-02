1d5daba3425d0d4a741452f105143eb5eb61caba
root <peterglad1985@hotmail.com>
2018-10-02 18:20:50 +0200

    dsf

--- rebuild-dc_madbot.py
+++ rebuild-dc_madbot.py
@@ -2,16 +2,11 @@
 
 import os
 
-#remove currect container and image
-os.system('docker stop $(docker ps -a | grep dc_madbot | awk \'{ printf $1 }\')')
-os.system('docker rm $(docker ps -a | grep dc_madbot | awk \'{ printf $1 }\')')
-os.system('docker rmi $(docker images | grep dc_madbot | awk \'{ printf $3 }\')')
-
 #pull
 os.system('git pull')
 
-#build new image
-os.system('docker build -t dc_madbot:stasi -f direct_communicate_madbot-dockerfile .')
+#kill running process
+os.system("kill $(ps -f | grep -m 1 direct_communicate_madbot-madbot.py | awk '{ printf $2 }')")
 
-#start new container
-os.system('docker run -d dc_madbot:stasi')
+#build new image
+os.system('python3 /projects/stasi/containers/direct_communicate_madbot-madbot.py &')
